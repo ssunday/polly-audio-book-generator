749b787db19360761d3958d26f031f1141ea6ffc
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2020-02-26 10:45:16 -0600

    [LINT] minor refactoring

--- synthesize.py
+++ synthesize.py
@@ -1,5 +1,4 @@
 import boto3
-import os
 import yaml
 
 polly_client = boto3.Session().client('polly')
@@ -7,20 +6,17 @@ polly_client = boto3.Session().client('polly')
 config_file = open("config.yml", "r")
 config = yaml.load(config_file, Loader=yaml.BaseLoader)
 
-
-def synthesize_speech(key, text):
-    polly_client.start_speech_synthesis_task(
-       OutputS3BucketName=config["s3_bucket"],
-       OutputS3KeyPrefix=key,
-       VoiceId=config["voice_id"],
-       OutputFormat=config["output_format"],
-       LexiconNames=config["lexicons"],
-       Text=text,
-     )
-
-
 s3_key = input("Key of the file in S3 to generate to: ")
 text_file = input("Path of the book file: ")
 input_file = open(text_file, "r")
 
-synthesize_speech(s3_key, input_file.read())
+polly_client.start_speech_synthesis_task(
+  OutputS3BucketName=config["s3_bucket"],
+  OutputS3KeyPrefix=s3_key,
+  VoiceId=config["voice_id"],
+  OutputFormat=config["output_format"],
+  LexiconNames=config["lexicons"],
+  Text=input_file.read(),
+)
+
+print("Synthesis started...")
